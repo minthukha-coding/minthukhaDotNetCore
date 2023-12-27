784b9441f1a33b421e267ff2b1367e5d1f12f6fb
minthukha-coding <minthukha1239788@gmail.com>
2023-12-27 16:45:44 +0630

    Complete EfCore ConsoleAPP CRUD

--- LemonDotNetCore.ConsoleApp/EFCoreExamples/EFCoreExample.cs
+++ LemonDotNetCore.ConsoleApp/EFCoreExamples/EFCoreExample.cs
@@ -11,8 +11,14 @@ namespace LemonDotNetCore.ConsoleApp.EFCoreExamples
     public class EFCoreExample
     {
         private readonly AppDbContext _dbContext = new AppDbContext();
-        public void Run() {
-            Read();
+        public void Run()
+        {
+            //Delete(10);
+            //Edit(10);
+            //Create("2", "2", "22");
+            //Update(9, "hg", "dafa asdfas");
+            //Edit(9);
+            //Update(111111, "fsaeHfeoasdfa el", "32r2sdadf", "gsdac Hello Myanmar");
         }
         private void Read()
         {
@@ -24,6 +30,66 @@ namespace LemonDotNetCore.ConsoleApp.EFCoreExamples
                 Console.WriteLine(item.Blog_Author);
                 Console.WriteLine(item.Blog_Content);
             }
+        }
+        public void Edit(int id)
+        {
+            BlogDataModel? item = _dbContext.Blogs.FirstOrDefault(x => x.Blog_Id == id);
+            if (item == null)
+            {
+                Console.WriteLine("No data not found!");
+                return;
+            }
+            Console.WriteLine(item.Blog_Id);
+            Console.WriteLine(item.Blog_Title);
+            Console.WriteLine(item.Blog_Author);
+            Console.WriteLine(item.Blog_Content);
+        }
+        public void Create(string title, string author, string content)
+        {
+            BlogDataModel blog = new BlogDataModel()
+            {
+                Blog_Title = title,
+                Blog_Author = author,
+                Blog_Content = content
+            };
+            _dbContext.Blogs.Add(blog);
+            int result = _dbContext.SaveChanges();
+            string message = result > 0 ? "Saving Successful." : "Saving Failed.";
+            Console.WriteLine(message);
+        }
+        private void Update(int id, string title, string author, string content)
+        {
+            BlogDataModel? item = _dbContext.Blogs.FirstOrDefault(x => x.Blog_Id == id);
+            if (item is null)
+            {
+                Console.WriteLine("No data found.");
+                return;
+            }
+
+            item.Blog_Title = title;
+            item.Blog_Author = author;
+            item.Blog_Content = content;
+
+            int result = _dbContext.SaveChanges();
+
+            string message = result > 0 ? "Updating Successful." : "Updating Failed.";
+            Console.WriteLine(message);
+        }
+
+        private void Delete(int id)
+        {
+            BlogDataModel? item = _dbContext.Blogs.FirstOrDefault(x => x.Blog_Id == id);
+            if (item is null)
+            {
+                Console.WriteLine("No data found.");
+                return;
+            }
+
+            _dbContext.Blogs.Remove(item);
+            int result = _dbContext.SaveChanges();
+
+            string message = result > 0 ? "Deleting Successful." : "Deleting Failed.";
+            Console.WriteLine(message);
         }
     }
 }
