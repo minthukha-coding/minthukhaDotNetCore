bad819bffad802262f07adcef8e720498b9f31b7
minthukha-coding <minthukha1239788@gmail.com>
2024-01-13 14:58:12 +0630

    Complete jQuery CRUD

--- LemonDotNetCore.HtmlApp/index.js
+++ LemonDotNetCore.HtmlApp/index.js
@@ -1,48 +1,115 @@
-const tblName = "Tbl_name";
+const tblName = "Tbl_Name";
+let _editId = null;
 
-$("#btn-save").click(function () {
+$("#btnSave").click(function () {
+  if (_editId == null) {
     createData();
+  } else {
+    updateData();
+  }
 });
 
 function createData() {
   let lst = [];
-
   if (localStorage.getItem(tblName) != null) {
     lst = JSON.parse(localStorage.getItem(tblName));
   }
 
-  const name = $("#textvalue").val();
+  const name = $("#txtName").val();
   const data = {
-    ID: uuidv4(),
+    Id: uuidv4(),
     Name: name,
   };
-
   lst.push(data);
   console.log({ lst });
   localStorage.setItem(tblName, JSON.stringify(lst));
+  // alert("Saving Successful.");
+
+  alert("Create Successful.");
+
+  $("#txtName").val("");
+  $("#txtName").focus();
+  readData();
 }
 
 function readData() {
   if (localStorage.getItem(tblName) == null) return;
 
-    var jsonStr = localStorage.getItem(tblName);
-    var lst = JSON.parse(jsonStr);
-    let trRows = '';
-    let count = 0;
-    console.log(lst);
-    lst.forEach(item => {
-        // console.log({item});
-        trRows +=  ` 
-      <tr>
-        <td></td>
-        <td>${++count}</td>
-        <td>${item.Name}</td>
-      </tr>`;
-    });
-    console.log(trRows);
+  var jsonStr = localStorage.getItem(tblName);
+  var lst = JSON.parse(jsonStr);
+  let trRows = "";
+  let count = 0;
+  lst.forEach((item) => {
+    trRows += `
+<tr>
+    <td>
+        <button type="button" class="btn btn-warning" onclick="editData('${
+          item.Id
+        }')">
+            <i class="fa-solid fa-pen-to-square"></i>
+        </button>
+        <button type="button" class="btn btn-danger" onclick="deleteData('${
+          item.Id
+        }')">
+            <i class="fa-solid fa-trash"></i>    
+        </button>
+    </td>
+    <td>${++count}</td>
+    <td>${item.Name}</td>
+</tr>`;
+  });
 
-    $('#table_data').html(trRows);
+  $("#tableData").html(trRows);
 }
+
+function editData(id) {
+  if (localStorage.getItem(tblName) == null) return;
+
+  var jsonStr = localStorage.getItem(tblName);
+  var lst = JSON.parse(jsonStr);
+  var results = lst.filter((x) => x.Id == id);
+  if (results.length == 0) {
+    alert("No data found.");
+    return;
+  }
+  var item = results[0];
+  _editId = item.Id;
+  $("#txtName").val(item.Name);
+  console.log(_editId);
+}
+
+function updateData() {
+  let lst = [];
+  if (localStorage.getItem(tblName) != null) {
+    lst = JSON.parse(localStorage.getItem(tblName));
+    console.log({ lst });
+  }
+
+  let index = lst.findIndex((x) => x.Id == _editId);
+  lst[index].Name = $("#txtName").val();
+
+  console.log({ lst });
+  localStorage.setItem(tblName, JSON.stringify(lst));
+  alert("Updating Successful.");
+  $("#txtName").val('');
+  $("#txtName").focus();
+  _editId = null;
+  readData();
+}
+
+function deleteData(id) {
+  let result = confirm('Are you sure want to delete?');
+  if (!result) return;
+    let lst = [];
+    if (localStorage.getItem(tblName) != null) {
+      lst = JSON.parse(localStorage.getItem(tblName));
+    }
+
+    lst = lst.filter((x) => x.Id != id);
+    localStorage.setItem(tblName, JSON.stringify(lst));
+    alert("Deleting Successful.");
+    readData();
+  };
 
 function uuidv4() {
   return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
@@ -53,4 +120,4 @@ function uuidv4() {
   );
 }
 
-read();
+readData();
