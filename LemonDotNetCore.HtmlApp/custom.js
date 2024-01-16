09103dc58e4db8ae0dc3fc7fa0115c004f84bebe
minthukha-coding <minthukha1239788@gmail.com>
2024-01-16 21:02:54 +0630

    Notiflix uses

--- LemonDotNetCore.HtmlApp/custom.js
+++ LemonDotNetCore.HtmlApp/custom.js
@@ -1,21 +1,51 @@
 function successMessage(message) {
-  Swal.fire({
-    title: "Success",
-    text: message,
-    icon: "success",
-  });
+  //sweetalert
+  // Swal.fire({
+  //     title: "Success",
+  //     text: message,
+  //     icon: "success"
+  // });
+
+  // alert(message);
+
+  Notiflix.Report.success(
+      "Notiflix Success",
+      message
+    );
 }
 
 function confirmMessage(message) {
-  return new Promise(function (myResolve, myReject) {
-    Swal.fire({
-      title: "Comfirm?",
-      text: message,
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonText: "Yes",
-    }).then((result) => {
-        myResolve(result.isConfirmed);
-    });
-  });
-}
+  //notiflix
+  return new Promise(function (myResolve,myReject){
+   Notiflix.Confirm.show(
+       "Confirm",
+       message,
+       'Yes',
+       'No',
+       function okCb(){
+           myResolve(true);
+       },
+       function cancelCb(){
+           myResolve(false);
+       }
+     );
+})
+
+    //sweetalert
+    // return new Promise(function (myResolve, myReject) {
+    //     Swal.fire({
+    //         title: "Confirm",
+    //         text: message,
+    //         icon: "warning",
+    //         showCancelButton: true,
+    //         confirmButtonText: "Yes"
+    //     }).then((result) => {
+    //         myResolve(result.isConfirmed);
+    //     });
+    // });
+
+    // return new Promise(function (myResolve, myReject) {
+    //     const result = confirm(message);
+    //     myResolve(result);
+    // });
+  }
