972cb319d86d54b1308c0332d508855918d037bd
minthukha-coding <minthukha1239788@gmail.com>
2024-02-07 00:57:22 +0630

    Complete MVC ( Read )

---
+++ LemonDotNetCore.MvcApp/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using LemonDotNetCore.MvcApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LemonDotNetCore.RestAPI
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<BlogDataModel> Blogs { get; set; }
+    }
+}
---
+++ LemonDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using LemonDotNetCore.RestAPI;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LemonDotNetCore.MvcApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        public readonly AppDbContext _appDbContext;
+
+        public BlogController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var lst = await _appDbContext.Blogs.ToListAsync();
+            return View(lst);
+        }
+    }
+}
