23ff6982bf47441a001bc961b7ac1a7753449f86
minthukha-coding <minthukha1239788@gmail.com>
2024-02-08 14:01:20 +0630

    Complete ApexChart MixedChart

--- LemonDotNetCore.MvcApp/Models/BlogDataModel.cs
+++ LemonDotNetCore.MvcApp/Models/BlogDataModel.cs
@@ -16,5 +16,7 @@ namespace LemonDotNetCore.MvcApp.Models
     {
         public List<double> Income_colum_data { get; set; }
         public List<double> Cashflow_column_data { get; set; }
+        public List<int> Xaxis_categories { get; set; }
+        public List<int> Revenue_line { get; set; }
     }
 }
