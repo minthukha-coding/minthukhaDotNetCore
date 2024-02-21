92c3b6ac8fa607269cd06f401275e8b15ef02ee6
minthukha-coding <minthukha1239788@gmail.com>
2024-02-22 00:04:35 +0630

    Complete ChartAppUsingSiginalR

---
+++ LemonDotNetCore.ChartAppUsingSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace LemonDotNetCore.ChartAppUsingSignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task ServerReciveMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ClientReceiveMessage", user, message);
+        }
+    }
+}
---
+++ LemonDotNetCore.ChartAppUsingSignalR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LemonDotNetCore.ChartAppUsingSignalR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
