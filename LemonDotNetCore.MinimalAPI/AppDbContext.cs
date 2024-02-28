288e2fea56e4e42e70d2abc24ae8996a3719e11a
minthukha-coding <minthukha1239788@gmail.com>
2024-02-28 10:34:23 +0630

    [ Complete ] MinimalAPI

---
+++ LemonDotNetCore.MinimalAPI/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using LemonDotNetCore.RestAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonDotNetCore.RestAPI
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+            {
+                DataSource = ".",
+                InitialCatalog = "LemonDotNetCore",
+                UserID = "sa",
+                Password = "sasasu",
+                TrustServerCertificate = true,
+            };
+
+            optionsBuilder.UseSqlServer(sqlConnectionStringBuilder.ConnectionString);
+        }
+        public DbSet<BlogDataModel> Blogs { get; set; }
+    }
+}
